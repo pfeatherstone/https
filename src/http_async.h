a0651498e22147ccb2ad94c9c3b97b388f9cd7e7
pfeatherstone <pfeatherstone@pf>
2025-04-25 14:03:46 +0100

    merge

--- src/http_async.h
+++ src/http_async.h
@@ -61,7 +61,6 @@ namespace http
     auto async_ws_accept (
         AsyncWriteStream&   sock,
         request             req,
-        request             req,
         CompletionToken&&   token
     );
 
@@ -366,8 +365,6 @@ namespace http
             {
                 state = writing;
 
-                // Get key
-                auto sec_ws_key = req.find(field::sec_websocket_key);
                 // Get key
                 auto sec_ws_key = req.find(field::sec_websocket_key);
 
