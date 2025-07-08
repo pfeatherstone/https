5b822e57fe82b731e01b9c7bea1e9e28a604bd83
me <me@me>
2025-07-08 08:22:22 +0100

    more cov

---
+++ examples/fuzz/sha1.cpp
@@ -0,0 +1,9 @@
+#include <cstdint>
+#include <cstddef>
+#include <http.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    auto sum = http::sha1{}.push(size, data).finish();
+    return 0;
+}
