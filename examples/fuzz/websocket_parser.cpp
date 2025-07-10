cd69cc4b5a8d9eb4d5182ed1fac18cf81e72f060
pfeatherstone <pfeatherstone@pf>
2025-07-10 18:24:51 +0100

    bit more cov

--- examples/fuzz/websocket_parser.cpp
+++ examples/fuzz/websocket_parser.cpp
@@ -13,11 +13,16 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
     bool is_websocket_req = req.is_websocket_req();
 
     std::vector<char> msg;
+    http::dynamic_buffer view(msg);
     http::websocket_parser ws_parser;
-    ws_parser.parse(msg, buf, ec);
+    ws_parser.parse(view, buf, ec);
     const auto opcode       = ws_parser.get_opcode();
     const auto is_server    = ws_parser.is_server();
-
+    const auto current_size = view.size();
+    const auto current_data = view.data();
+    const auto current_buf  = view.buffer();
+    view.resize(10);
+    view.clear();
     msg.assign(data, data+size);
     buf.clear();
     http::serialize_websocket_message(boost::asio::buffer(msg), http::WS_OPCODE_DATA_BINARY, true, buf);
