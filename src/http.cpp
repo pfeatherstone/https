304b5d0c2a9299a9830512dfe259061e7818f953
pfeatherstone <pfeatherstone@pf>
2025-07-12 13:04:38 +0100

    better cov ?

--- src/http.cpp
+++ src/http.cpp
@@ -468,82 +468,85 @@ namespace http
 
 //----------------------------------------------------------------------------------------------------------------
 
-    std::string_view status_label(const status_type v)
-    {
-        switch(v)
-        {
+    constexpr std::pair<status_type, std::string_view> STATUS_LABELS[] = {
         // 1xx
-        case status_type::continue_:                             return "Continue";
-        case status_type::switching_protocols:                   return "Switching Protocols";
-        case status_type::processing:                            return "Processing";
-        case status_type::early_hints:                           return "Early Hints";
+        {status_type::continue_,                       "Continue"},
+        {status_type::switching_protocols,             "Switching Protocols"},
+        {status_type::processing,                      "Processing"},
+        {status_type::early_hints,                     "Early Hints"},
 
         // 2xx
-        case status_type::ok:                                    return "OK";
-        case status_type::created:                               return "Created";
-        case status_type::accepted:                              return "Accepted";
-        case status_type::non_authoritative_information:         return "Non-Authoritative Information";
-        case status_type::no_content:                            return "No Content";
-        case status_type::reset_content:                         return "Reset Content";
-        case status_type::partial_content:                       return "Partial Content";
-        case status_type::multi_status:                          return "Multi-Status";
-        case status_type::already_reported:                      return "Already Reported";
-        case status_type::im_used:                               return "IM Used";
+        {status_type::ok,                              "OK"},
+        {status_type::created,                         "Created"},
+        {status_type::accepted,                        "Accepted"},
+        {status_type::non_authoritative_information,   "Non-Authoritative Information"},
+        {status_type::no_content,                      "No Content"},
+        {status_type::reset_content,                   "Reset Content"},
+        {status_type::partial_content,                 "Partial Content"},
+        {status_type::multi_status,                    "Multi-Status"},
+        {status_type::already_reported,                "Already Reported"},
+        {status_type::im_used,                         "IM Used"},
 
         // 3xx
-        case status_type::multiple_choices:                      return "Multiple Choices";
-        case status_type::moved_permanently:                     return "Moved Permanently";
-        case status_type::found:                                 return "Found";
-        case status_type::see_other:                             return "See Other";
-        case status_type::not_modified:                          return "Not Modified";
-        case status_type::use_proxy:                             return "Use Proxy";
-        case status_type::temporary_redirect:                    return "Temporary Redirect";
-        case status_type::permanent_redirect:                    return "Permanent Redirect";
+        {status_type::multiple_choices,                "Multiple Choices"},
+        {status_type::moved_permanently,               "Moved Permanently"},
+        {status_type::found,                           "Found"},
+        {status_type::see_other,                       "See Other"},
+        {status_type::not_modified,                    "Not Modified"},
+        {status_type::use_proxy,                       "Use Proxy"},
+        {status_type::temporary_redirect,              "Temporary Redirect"},
+        {status_type::permanent_redirect,              "Permanent Redirect"},
 
         // 4xx
-        case status_type::bad_request:                           return "Bad Request";
-        case status_type::unauthorized:                          return "Unauthorized";
-        case status_type::payment_required:                      return "Payment Required";
-        case status_type::forbidden:                             return "Forbidden";
-        case status_type::not_found:                             return "Not Found";
-        case status_type::method_not_allowed:                    return "Method Not Allowed";
-        case status_type::not_acceptable:                        return "Not Acceptable";
-        case status_type::proxy_authentication_required:         return "Proxy Authentication Required";
-        case status_type::request_timeout:                       return "Request Timeout";
-        case status_type::conflict:                              return "Conflict";
-        case status_type::gone:                                  return "Gone";
-        case status_type::length_required:                       return "Length Required";
-        case status_type::precondition_failed:                   return "Precondition Failed";
-        case status_type::payload_too_large:                     return "Payload Too Large";
-        case status_type::uri_too_long:                          return "URI Too Long";
-        case status_type::unsupported_media_type:                return "Unsupported Media Type";
-        case status_type::range_not_satisfiable:                 return "Range Not Satisfiable";
-        case status_type::expectation_failed:                    return "Expectation Failed";
-        case status_type::i_am_a_teapot:                         return "I'm a teapot";
-        case status_type::misdirected_request:                   return "Misdirected Request";
-        case status_type::unprocessable_entity:                  return "Unprocessable Entity";
-        case status_type::locked:                                return "Locked";
-        case status_type::failed_dependency:                     return "Failed Dependency";
-        case status_type::too_early:                             return "Too Early";
-        case status_type::upgrade_required:                      return "Upgrade Required";
-        case status_type::precondition_required:                 return "Precondition Required";
-        case status_type::too_many_requests:                     return "Too Many Requests";
-        case status_type::request_header_fields_too_large:       return "Request Header Fields Too Large";
-        case status_type::unavailable_for_legal_reasons:         return "Unavailable For Legal Reasons";
+        {status_type::bad_request,                     "Bad Request"},
+        {status_type::unauthorized,                    "Unauthorized"},
+        {status_type::payment_required,                "Payment Required"},
+        {status_type::forbidden,                       "Forbidden"},
+        {status_type::not_found,                       "Not Found"},
+        {status_type::method_not_allowed,              "Method Not Allowed"},
+        {status_type::not_acceptable,                  "Not Acceptable"},
+        {status_type::proxy_authentication_required,   "Proxy Authentication Required"},
+        {status_type::request_timeout,                 "Request Timeout"},
+        {status_type::conflict,                        "Conflict"},
+        {status_type::gone,                            "Gone"},
+        {status_type::length_required,                 "Length Required"},
+        {status_type::precondition_failed,             "Precondition Failed"},
+        {status_type::payload_too_large,               "Payload Too Large"},
+        {status_type::uri_too_long,                    "URI Too Long"},
+        {status_type::unsupported_media_type,          "Unsupported Media Type"},
+        {status_type::range_not_satisfiable,           "Range Not Satisfiable"},
+        {status_type::expectation_failed,              "Expectation Failed"},
+        {status_type::i_am_a_teapot,                   "I'm a teapot"},
+        {status_type::misdirected_request,             "Misdirected Request"},
+        {status_type::unprocessable_entity,            "Unprocessable Entity"},
+        {status_type::locked,                          "Locked"},
+        {status_type::failed_dependency,               "Failed Dependency"},
+        {status_type::too_early,                       "Too Early"},
+        {status_type::upgrade_required,                "Upgrade Required"},
+        {status_type::precondition_required,           "Precondition Required"},
+        {status_type::too_many_requests,               "Too Many Requests"},
+        {status_type::request_header_fields_too_large, "Request Header Fields Too Large"},
+        {status_type::unavailable_for_legal_reasons,   "Unavailable For Legal Reasons"},
+
         // 5xx
-        case status_type::internal_server_error:                 return "Internal Server Error";
-        case status_type::not_implemented:                       return "Not Implemented";
-        case status_type::bad_gateway:                           return "Bad Gateway";
-        case status_type::service_unavailable:                   return "Service Unavailable";
-        case status_type::gateway_timeout:                       return "Gateway Timeout";
-        case status_type::http_version_not_supported:            return "HTTP Version Not Supported";
-        case status_type::variant_also_negotiates:               return "Variant Also Negotiates";
-        case status_type::insufficient_storage:                  return "Insufficient Storage";
-        case status_type::loop_detected:                         return "Loop Detected";
-        case status_type::not_extended:                          return "Not Extended";
-        case status_type::network_authentication_required:       return "Network Authentication Required";
-        default: break;
-        }
+        {status_type::internal_server_error,           "Internal Server Error"},
+        {status_type::not_implemented,                 "Not Implemented"},
+        {status_type::bad_gateway,                     "Bad Gateway"},
+        {status_type::service_unavailable,             "Service Unavailable"},
+        {status_type::gateway_timeout,                 "Gateway Timeout"},
+        {status_type::http_version_not_supported,      "HTTP Version Not Supported"},
+        {status_type::variant_also_negotiates,         "Variant Also Negotiates"},
+        {status_type::insufficient_storage,            "Insufficient Storage"},
+        {status_type::loop_detected,                   "Loop Detected"},
+        {status_type::not_extended,                    "Not Extended"},
+        {status_type::network_authentication_required, "Network Authentication Required"},
+    };
+
+    std::string_view status_label(const status_type s)
+    {
+        for (auto [k,l] : STATUS_LABELS)
+            if (k == s)
+                return l;
         return "<unknown-status>";
     }
 
