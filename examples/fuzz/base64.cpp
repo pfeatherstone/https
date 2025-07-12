52abb1a7299a04b3e508d119ef608950061a75c9
pfeatherstone <pfeatherstone@pf>
2025-07-12 12:55:03 +0100

    more cov

--- examples/fuzz/base64.cpp
+++ examples/fuzz/base64.cpp
@@ -1,10 +1,13 @@
 #include <cstdint>
 #include <cstddef>
+#include <algorithm>
 #include <http.h>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     auto encoded = http::base64_encode(size, data);
     auto decoded = http::base64_decode(encoded);
+    encoded.erase(std::remove(begin(encoded), end(encoded), '='), end(encoded));
+    auto decoded2 = http::base64_decode(encoded);
     return 0;
 }
